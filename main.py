de38bb9bce3e2fc1054186f2dae4b3ce70eb8e97
NesDoppio <panuwat.rujirawanich@gmail.com>
2024-08-07 23:01:41 +0700

    remove_unnessesary_header

--- main.py
+++ main.py
@@ -13,23 +13,7 @@ project_id = credential_data["project_id"]
 
 url = 'https://backend.humanos.biz/Admin/EnhanceLeaveListUI.aspx?MenuID=318'
 headers = {
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-encoding': 'gzip, deflate, br, zstd',
-    'accept-language': 'en-US,en;q=0.9',
-    'cache-control': 'max-age=0',
-    'connection': 'keep-alive',
     'cookie': f'ASP.NET_SessionId={credential_data["humanos_session_token"]}; DetectLoginHMOS=LoginUI=LoginUI',
-    'host': 'backend.humanos.biz',
-    'referer': 'https://backend.humanos.biz/Admin/MainUI.aspx?MenuID=100',
-    'sec-ch-ua': '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
 }
 
 response = requests.get(url, headers=headers)
